9d262951eeb7a9dcd023b1b9c733144426b46ff0
Wilfrid Azariah <m11002818@mail.ntust.edu.tw>
2025-03-12 13:24:40 +0800

    Update simu01.py

--- simu/simu01.py
+++ simu/simu01.py
@@ -1,6 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+##### Mathematical #####
+def compute_p_success(P_noise, P_attacker_TX, P_UE_TX, alpha, beta_values, j_max, G_gNB, G_UE, G_attacker, wavelength, gamma, d_UE, d_attacker):
+    # Compute received power based on channel model
+    P_UE = P_UE_TX * G_UE * G_gNB * (wavelength / (4 * np.pi))**2 * (1 / d_UE)**gamma
+    P_attacker = P_attacker_TX * G_attacker * G_gNB * (wavelength / (4 * np.pi))**2 * (1 / d_attacker)**gamma
+    
+    j_range = np.arange(1, j_max + 1)
+    results_P_S = {}
+    results_P_noise_j1 = {}
+    
+    for beta in beta_values:
+        P_noise_values = [P_noise]
+        
+        for i in range(1, j_max + 1):
+            if (i - 2) % (1 / beta) == 0 and i > 1:
+                P_next = (1 - alpha) * P_noise_values[-1] + alpha * P_attacker
+            else:
+                P_next = (1 - alpha) * P_noise_values[-1] + alpha * P_noise
+            
+            P_noise_values.append(P_next)
+        
+        P_S = [1 if P_UE > P_noise_values[j] else 0 for j in range(j_max)]
+        results_P_S[beta] = P_S
+
+        P_noise_j1 = [P_noise_values[j] for j in range(j_max)]
+        results_P_noise_j1[beta] = P_noise_j1
+    
+    return j_range, results_P_S, results_P_noise_j1
+
+##### Simulation #####
 class UE:
     def __init__(self, power):
         self.power = power  # UE Msg1 power
@@ -52,6 +82,7 @@ def simulate(j_max, P_noise, P_attacker, P_UE, alpha, beta_values):
     
     return j_range, results_P_S, results_P_noise_j1
 
+##### Main Program #####
 # Given parameters
 P_noise = 28  # dB
 P_attacker = 55  # dB
@@ -60,14 +91,19 @@ alpha = 0.1
 beta_values = [1, 0.5, 0.25, 0.125]
 j_max = 110
 
+# Compute results
+j_range, math_P_S, math__P_noise_j1 = compute_p_success(P_noise, P_attacker_TX, P_UE_TX, alpha, beta_values, j_max, G_gNB, G_UE, G_attacker, wavelength, gamma, d_UE, d_attacker)
+
 # Run simulation
-j_range, results_P_S, results_P_noise_j1 = simulate(j_max, P_noise, P_attacker, P_UE, alpha, beta_values)
+j_range, simu_P_S, simu_P_noise_j1 = simulate(j_max, P_noise, P_attacker, P_UE, alpha, beta_values)
 
 # Plot results
 plt.figure(figsize=(12, 6))
 plt.subplot(1, 2, 1)
-for beta, P_S in results_P_S.items():
-    plt.plot(j_range, P_S, label=f'β = {beta}')
+for beta, P_S in math_P_S.items():
+    plt.plot(j_range, P_S, linestyle='-', label=f'β = {beta}')
+for beta, P_S in simu_P_S.items():
+    plt.plot(j_range, P_S, linestyle='none', marker='.', label=f'β = {beta} sim')
 
 plt.xlabel("j (RAO Early Start)")
 plt.ylabel("P_S (Msg1 Success Probability)")
@@ -76,8 +112,10 @@ plt.legend()
 plt.grid()
 
 plt.subplot(1, 2, 2)
-for beta, P_noise_j1 in results_P_noise_j1.items():
-    plt.plot(j_range, P_noise_j1, label=f'β = {beta}')
+for beta, P_noise_j1 in math_P_noise_j1.items():
+    plt.plot(j_range, P_noise_j1, linestyle='-', label=f'β = {beta}')
+for beta, P_noise_j1 in simu_P_noise_j1.items():
+    plt.plot(j_range, P_noise_j1, linestyle='none', marker='.', label=f'β = {beta} sim')
 
 plt.axhline(P_UE, color='red', ls='dotted')
 plt.xlabel("j (RAO Early Start)")
