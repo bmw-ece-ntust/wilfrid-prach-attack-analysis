51f329960698c52297a4b6c9fc4357c9667c5835
Wilfrid Azariah <m11002818@mail.ntust.edu.tw>
2025-03-14 16:20:20 +0800

    Update model01.py

--- math/model01.py
+++ math/model01.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def compute_p_success(P_noise, P_attacker, P_UE, alpha, Ta_values, j_max):
-    j_range = np.arange(1, j_max + 1)
+def compute_p_success(P_noise, P_attacker, P_UE, alpha, delta, Ta_values, j_max):
+    j_range = np.arange(0, j_max + 1)
     results_P_S = {}
     results_P_noise_j1 = {}
     
@@ -17,7 +17,7 @@ def compute_p_success(P_noise, P_attacker, P_UE, alpha, Ta_values, j_max):
             
             P_noise_values.append(P_next)
         
-        P_S = [1 if P_UE > P_noise_values[j] else 0 for j in range(j_max)]
+        P_S = [1 if P_UE > (P_noise_values[j] + delta) else 0 for j in range(j_max)]
         results_P_S[Ta] = P_S
 
         P_noise_j1 = [P_noise_values[j] for j in range(j_max)]
@@ -26,15 +26,16 @@ def compute_p_success(P_noise, P_attacker, P_UE, alpha, Ta_values, j_max):
     return j_range, results_P_S, results_P_noise_j1
 
 # Given parameters
-P_noise = 28  # dB
+P_noise = 17.4  # dB
 P_attacker = 55  # dB
 P_UE = 54  # dB
 alpha = 0.1
+delta = 12
 Ta_values = [1, 2, 4, 8]
-j_max = 110
+j_max = 42
 
 # Compute results
-j_range, results_P_S, results_P_noise_j1 = compute_p_success(P_noise, P_attacker, P_UE, alpha, Ta_values, j_max)
+j_range, results_P_S, results_P_noise_j1 = compute_p_success(P_noise, P_attacker, P_UE, alpha, delta, Ta_values, j_max)
 
 # Plot results
 plt.figure(figsize=(12, 6))
@@ -52,7 +53,7 @@ plt.subplot(1, 2, 2)
 for Ta, P_noise_j1 in results_P_noise_j1.items():
     plt.plot(j_range, P_noise_j1, label=f'Ta = {Ta}')
 
-plt.axhline(P_UE, color='red', ls='dotted', label=f'P_UE')
+plt.axhline(P_UE - delta, color='red', ls='dotted', label=f'P_UE - delta')
 plt.xlabel("j (RAO Early Start)")
 plt.ylabel("P_noise_j1 (gNB's Noise Threshold at j+1)")
 plt.title("gNB's Noise Threshold at j+1 vs j for Different Ta Values")
