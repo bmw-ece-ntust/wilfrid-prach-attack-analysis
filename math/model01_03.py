acb127cfeb5fc8b4a0605a715b2630c6be4f748e
Wilfrid Azariah <m11002818@mail.ntust.edu.tw>
2025-03-17 10:13:09 +0800

    Update model01_03.py

--- math/model01_03.py
+++ math/model01_03.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def compute_p_success(P_noise, P_attacker, P_UE, alpha, delta, Ta_values, j_max):
+def compute_p_success(P_noise, P_attacker, P_UE, alpha, delta_values, Ta, j_max):
     j_range = np.arange(0, j_max)
     results_P_S = {}
     results_P_noise_j1 = {}
     
-    for Ta in Ta_values:
+    for delta in delta_values:
         P_noise_values = [P_noise]
         
         for i in range(1, j_max + 1):
@@ -18,10 +18,10 @@ def compute_p_success(P_noise, P_attacker, P_UE, alpha, delta, Ta_values, j_max)
             P_noise_values.append(P_next)
         
         P_S = [1 if P_UE > (P_noise_values[j] + delta) else 0 for j in range(j_max)]
-        results_P_S[Ta] = P_S
+        results_P_S[delta] = P_S
 
         P_noise_j1 = [P_noise_values[j] for j in range(j_max)]
-        results_P_noise_j1[Ta] = P_noise_j1
+        results_P_noise_j1[delta] = P_noise_j1
     
     return j_range, results_P_S, results_P_noise_j1
 
@@ -30,30 +30,30 @@ P_noise = 17.4  # dB
 P_attacker = 55  # dB
 P_UE = 54  # dB
 alpha = 0.12
-delta = 12
-Ta_values = [1, 2, 4, 8]
+delta_values = [14,12,10,8]
+Ta_values = 1
 j_max = 42
 
 # Compute results
-j_range, results_P_S, results_P_noise_j1 = compute_p_success(P_noise, P_attacker, P_UE, alpha, delta, Ta_values, j_max)
+j_range, results_P_S, results_P_noise_j1 = compute_p_success(P_noise, P_attacker, P_UE, alpha, delta_values, Ta, j_max)
 
 # Plot results
 plt.figure(figsize=(12, 6))
 plt.subplot(1, 2, 1)
-for Ta, P_S in results_P_S.items():
-    plt.plot(j_range, P_S, label=f'Ta = {Ta}')
+for delta, P_S in results_P_S.items():
+    plt.plot(j_range, P_S, label=f'delta = {delta}')
 
 plt.xlabel("j (RAO Early Start)")
 plt.ylabel("P_S (Msg1 Success Probability)")
-plt.title("UE Msg1 Success Probability vs j for Different Ta Values")
+plt.title("UE Msg1 Success Probability vs j for Different delta Values")
 plt.legend()
 plt.grid()
 
 plt.subplot(1, 2, 2)
-for Ta, P_noise_j1 in results_P_noise_j1.items():
-    plt.plot(j_range, P_noise_j1, label=f'Ta = {Ta}')
-
-plt.axhline(P_UE - delta, color='red', ls='dotted', label=f'P_UE - delta')
+for delta, P_noise_j1 in results_P_noise_j1.items():
+    plt.plot(j_range, P_noise_j1, label=f'delta = {delta}')
+for delta in delta_values:
+    plt.axhline(P_UE - delta, ls='dotted', label=f'P_UE - {delta}')
 plt.xlabel("j (RAO Early Start)")
 plt.ylabel("P_noise_j1 (gNB's Noise Threshold at j+1)")
 plt.title("gNB's Noise Threshold at j+1 vs j for Different Ta Values")
