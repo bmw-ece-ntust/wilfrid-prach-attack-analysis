2d455d759068c998c817c0d74eeaffdf46fd4663
Wilfrid Azariah <m11002818@mail.ntust.edu.tw>
2025-03-17 09:40:00 +0800

    Update model01_02.py

--- math/model01_02.py
+++ math/model01_02.py
@@ -29,7 +29,7 @@ def compute_p_success(P_noise, P_attacker, P_UE, alpha_values, delta, Ta, j_max)
 P_noise = 17.4  # dB
 P_attacker = 55  # dB
 P_UE = 54  # dB
-alpha_values = [0.14,0.12,0.1,0.8]
+alpha_values = [0.14,0.12,0.1,0.08]
 delta = 12
 Ta = 1
 j_max = 42
