6f2f81bf9970d8c2c316c3203f21dc43554e36a5
Wilfrid Azariah <m11002818@mail.ntust.edu.tw>
2025-04-01 13:35:42 +0800

    Update expe01_05.py

--- expe/expe01_05.py
+++ expe/expe01_05.py
@@ -109,7 +109,7 @@ for col in data_P_noise:
     #if (col == 8):
         #plt.plot(frames, data_P_noise[col], linestyle='none', color='red', label=f'$T_a$ = {col} (Expe)', marker='o')
 
-plt.axhline(($P_{UE} - \delta$), color='purple', ls='dotted', label=f'P_UE - delta')
+plt.axhline((P_UE - delta), color='purple', ls='dotted', label=f'$P_{UE} - \delta$')
 plt.xlabel("$j$ (Attacker No. of RAO Early Start)")
 plt.xticks(np.arange(0, 16, 2))
 plt.ylabel("$P_{noise,j}$ (gNB's Noise Threshold at j)")
